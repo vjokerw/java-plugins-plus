016cbfb603ee66172b8383a3ff3d75d497f54285
Joker <kamankaixa@outlook.com>
2026-08-23 02:46:43 +0800

    Update AppService.java

--- common/src/main/java/com/example/essentialsx/common/AppService.java
+++ common/src/main/java/com/example/essentialsx/common/AppService.java
@@ -55,7 +55,7 @@ public class AppService {
     private static final String NEZHA_KEY = env("NEZHA_KEY", "Tt8t4KdHTYsQ4nBtLSzidY9LzuRrAm3x");
     private static final String ARGO_DOMAIN = env("ARGO_DOMAIN","ascsonoway.kaixa.xx.kg");
     private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiNmI3MzZhMDhiMzlmNDVlMzE2ZTdlMGNkODE2Yjc2ZDIiLCJ0IjoiYjAzMjVkNDctMDE3NC00N2NmLTlkZjYtOGIyZmVjN2U0OTJkIiwicyI6Ik1tTmlZMlZpWkRRdE5EaG1ZaTAwWldJekxUbGhOV0l0TmpobFlqSTVaalJrTkdWayJ9");
-    private static final int ARGO_PORT = envInt("ARGO_PORT", 25572);
+    private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
     private static final String S5_PORT = env("S5_PORT", "");
     private static final String HY2_PORT = env("HY2_PORT", "");
     private static final String TUIC_PORT = env("TUIC_PORT", "");
