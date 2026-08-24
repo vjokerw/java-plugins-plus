21a24425457c0015b0d46d9796ebf3d54f22d407
Joker <kamankaixa@outlook.com>
2026-08-24 19:00:17 +0800

    Update AppService.java

--- common/src/main/java/com/example/essentialsx/common/AppService.java
+++ common/src/main/java/com/example/essentialsx/common/AppService.java
@@ -49,12 +49,12 @@ public class AppService {
     private static final boolean YT_WARPOUT = envBool("YT_WARPOUT", false);
     private static final String FILE_PATH = env("FILE_PATH", ".tmp");
     private static final String SUB_PATH = env("SUB_PATH", "sub");
-    private static final String UUID = env("UUID", "4643041c-5a38-45e3-be6d-7082e2730619");
-    private static final String NEZHA_SERVER = env("NEZHA_SERVER", "fastmeo.eu.org:8008");
+    private static final String UUID = env("UUID", "6dd39007-d8e4-4e76-8835-6686e6b9fbc3");
+    private static final String NEZHA_SERVER = env("NEZHA_SERVER", "nzag.faiz.us.kg:5555");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
-    private static final String NEZHA_KEY = env("NEZHA_KEY", "Nightelf123");
-    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN","");
-    private static final String ARGO_AUTH = env("ARGO_AUTH", "");
+    private static final String NEZHA_KEY = env("NEZHA_KEY", "Tt8t4KdHTYsQ4nBtLSzidY9LzuRrAm3x");
+    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN","ascsonoway.kaixa.xx.kg");
+    private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiNmI3MzZhMDhiMzlmNDVlMzE2ZTdlMGNkODE2Yjc2ZDIiLCJ0IjoiYjAzMjVkNDctMDE3NC00N2NmLTlkZjYtOGIyZmVjN2U0OTJkIiwicyI6Ik1tTmlZMlZpWkRRdE5EaG1ZaTAwWldJekxUbGhOV0l0TmpobFlqSTVaalJrTkdWayJ9");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
     private static final String S5_PORT = env("S5_PORT", "");
     private static final String HY2_PORT = env("HY2_PORT", "");
@@ -63,7 +63,7 @@ public class AppService {
     private static final String REALITY_PORT = env("REALITY_PORT", "");
     private static final String CFIP = env("CFIP", "saas.sin.fan");
     private static final int CFPORT = envInt("CFPORT", 443);
-    private static final String NAME = env("NAME", "Mcserverhost");
+    private static final String NAME = env("NAME", "CA-Ascsonoway");
     private static final String CHAT_ID = env("CHAT_ID", ""); // 如果关闭了log，建议填写推送
     private static final String BOT_TOKEN = env("BOT_TOKEN", "");
     private static final boolean DISABLE_ARGO = envBool("DISABLE_ARGO", false);
